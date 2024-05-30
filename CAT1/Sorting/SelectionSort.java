8fa488d1a7119e010fbc68fc650a5b43e95368cf
Uday Kumar <94821907+udayvelupuri1@users.noreply.github.com>
2024-05-30 09:47:50 +0530

    Update SelectionSort.java

--- CAT1/Sorting/SelectionSort.java
+++ CAT1/Sorting/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for(int i=0;i<n;i++){
             int minIndex = i;
             for(int j=0;j<n;j++){
-                if(arr[j]< arr[minIndex]){
+                if(arr[j]> arr[minIndex]){
                     minIndex =j;
                 }
             }
@@ -22,7 +22,7 @@ public class SelectionSort {
             arr[i] = sc.nextInt();
         }
         sort(arr,n);
-        for(int i=n-1;i>0;i--){
+        for(int i=0;i<n;i++){
             System.out.print(arr[i]+ " ");
         }
     }
